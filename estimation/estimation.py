8f179d351177cf538b425a46860e81ff2a36c85e
rickwg <rick.wilming@fu-berlin.de>
2015-06-25 14:11:56 +0200

    added transition estimation for reversible matrices

--- estimation/estimation.py
+++ estimation/estimation.py
@@ -43,3 +43,45 @@ def estimate_transition_matrix_naiv(i_count_mat):
         row = i_count_mat[i, :]
         est_trans_mat[i, :] = np.float64(row) / np.sum(np.float64(row))
     return est_trans_mat
+
+def estimate_transition_matrix(i_count_mat, i_x0, i_max_iter, i_tol):
+    """
+    Estimate transition matrix with detailed balance condition
+
+    Parameters
+    ----------
+    i_count_mat : numpy.array, count matrix
+    i_max_iter: float, max number of iterations
+    i_tol: float, tolerance
+
+    Returns
+    -------
+    est_trans_mat : numpy.array
+
+    """
+    n = i_count_mat.shape[0]
+    numerator = i_count_mat + i_count_mat.T
+    count_mat_row_sum = np.sum(i_count_mat, axis=1)
+    count_mat_col_sum = np.sum(i_count_mat, axis=0)
+    x = i_x0
+    iterations = 0
+    denominator = np.zeros(x.shape)
+
+    while iter < i_max_iter:
+        x_row_sum = np.sum(x, axis=1)
+        # x_col_sum = np.sum(x, axis=0)
+
+        # q_i = c_i / x_i
+        q_i = count_mat_row_sum / x_row_sum
+        q_j = count_mat_col_sum / x_row_sum
+
+        for i in xrange(n):
+            for j in xrange(n):
+                denominator[i, j] = q_i[i] + q_j[j]
+        x = numerator / denominator
+
+        iterations += 1
+
+    est_trans_mat = x / np.sum(x, axis=1)
+    return est_trans_mat
+
