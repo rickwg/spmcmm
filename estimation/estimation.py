016d5993737943e48e5bed39d15277915685843b
rickwg <rick.wilming@fu-berlin.de>
2015-06-25 11:42:26 +0200

    added comments

--- estimation/estimation.py
+++ estimation/estimation.py
@@ -1,20 +1,45 @@
-
 import numpy as np
 
+
 def compute_count_matrix(i_chain):
+    """
+    Compute count matrix of a markov chain
+
+    Parameters
+    ----------
+    i_chain : numpy.array that contains a discrete markov chain
+
+
+    Returns
+    -------
+    count_matrix : numpy.array
+
+    """
+
     n_states = i_chain.max() + 1
     count_matrix = np.zeros((n_states, n_states), dtype=np.intc)
 
     for i in xrange(1, i_chain.shape[0]):
-        count_matrix[i_chain[i-1], i_chain[i]] += 1
+        count_matrix[i_chain[i - 1], i_chain[i]] += 1
     return count_matrix
 
+
 def estimate_transition_matrix(i_count_mat):
+    """
+    Estimate transition matrix
+
+    Parameters
+    ----------
+    i_count_mat : numpy.array
+
+    Returns
+    -------
+    est_trans_mat : numpy.array
+
+    """
     est_trans_mat = np.zeros(i_count_mat.shape, dtype=np.float64)
 
     for i in xrange(est_trans_mat.shape[0]):
         row = i_count_mat[i, :]
         est_trans_mat[i, :] = np.float64(row) / np.sum(np.float64(row))
     return est_trans_mat
-
-
