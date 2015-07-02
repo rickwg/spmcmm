7ee53a8820e15b95cdd9450feae83a9ca6c231bc
rickwg <rick.wilming@fu-berlin.de>
2015-07-02 12:45:19 +0200

    added setup script

---
+++ setup.py
@@ -0,0 +1,13 @@
+"""This is the setup for the spmcmm package"""
+
+from setuptools import setup
+
+setup(
+    name='spmcmm',
+    version="1.0",
+    license='',
+    setup_requires=['numpy>=1.7.1'],
+    tests_require=['numpy>=1.7.1', 'nose>=1.3'],
+    install_requires=['numpy>=1.7.1'],
+    packages=['clustering', 'estimation', 'analysis'],
+    test_suite='nose.collector')
