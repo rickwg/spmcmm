5ed20e7384c9c7e317d9a0f5d4146b81a6a5c879
rickwg <rick.wilming@fu-berlin.de>
2015-07-02 12:18:48 +0200

    adapted tests

--- estimation/test/test_estimation.py
+++ estimation/test/test_estimation.py
@@ -8,19 +8,16 @@ import numpy as np
 
 def test_compute_count_mat():
     """Testing computation of count matrix"""
-    check_mat = np.array([[0, 3, 0], [0, 0, 3], [3, 0, 0]], dtype=np.intc)
-
-    trans_mat = np.array([[0, 1, 0], [0, 0, 1], [1, 0, 0]], dtype=np.intc)
-    chain = nsampl.evolve_chain(0, trans_mat, 10)
+    check_mat = np.array([[0, 7, 0], [0, 0, 6], [6, 0, 0]])
+    chain = np.array([0, 1, 2, 0, 1, 2, 0, 1, 2, 0, 1, 2, 0, 1, 2, 0, 1, 2, 0, 1])
     count_mat = est.compute_count_matrix(chain)
 
     assert_true(count_mat == check_mat)
 
 def test_est_trans_matrix_naive():
     """Testing estimation of transition matrix"""
-    trans_mat = np.array([[0, 1, 0], [0, 0, 1], [1, 0, 0]], dtype=np.intc)
-    chain = nsampl.evolve_chain(0, trans_mat, 10)
-    count_mat = est.compute_count_matrix(chain)
+    trans_mat = np.array([[0, 1, 0], [0, 0, 1], [1, 0, 0]])
+    count_mat = np.array([[0, 7, 0], [0, 0, 6], [6, 0, 0]])
     est_trans_mat = est.estimate_transition_matrix_naive(count_mat)
 
     assert_true(est_trans_mat == trans_mat)
