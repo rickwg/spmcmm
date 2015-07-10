604f2ae0e162fe0856f5285b22885d15a70a92a2
JoschkaB <joschka-b@web.de>
2015-07-10 08:43:39 +0200

    faster now and with comments

--- clustering/edit.py
+++ clustering/edit.py
@@ -1,41 +1,52 @@
 import numpy as np
 import random
+from scipy.spatial import distance
 
 class kMeans:
-    def __init__(self, data, k=1):
-        """Parameters
+    def __init__(self, data, k=1, maxiter = 1000):
+        """
+        Class for K-Means calculation
+        Parameters
         ----------
-        X :a NxD matrix.
-        N :the number of samples.
-        D :the dimension of each sample.
-        k :the number of centers to choose.
-        maxiter :the maximum of iterations."""
-        #self.centers = self.__random_centers(X, k)
+        data : numpy.array that contains a D dimensional time series
+        k : int, default: 1 -  the number of clusters to generate
+        maxiter: int, default: 1000 - Maximum number of iterations
 
-        N,D = data.shape
+        -------
+        centers : numpy.array with the calculated centers
+        labels : numpy.array with lables of each point
+        """
+
+        N, D = data.shape
         self.centers = np.zeros((k, D))
         self.labels = np.zeros((N, 1))
-        self.maxiter = 5000
+        self.maxiter = maxiter
         self.k = k
         self.data = data
         self.centerIsSet = False
 
 
     def __random_centers(self):
-        """Using the forgy method to chose firstly the initial k centers randomly from N samples.
-        kcenters:a kxD matrix to instore the first k centers."""
+        """
+        Compute initial k centers rondomly
+        Parameters
+        ----------
+
+        Returns
+        -------
+        centers : numpy.array with the initial centers
+        """
         kcenters = random.sample(self.data[:, :], self.k)
         kcenters = np.asarray(kcenters)
-        print kcenters
         return kcenters
 
-    def set_maxiter(self ,maxiter):
+    def set_maxiter(self, maxiter):
         self.maxiter = maxiter
 
     def set_centers(self ,centers):
         self.centers = centers
         self.centerIsSet = True
-        _, self.k = centers.shape
+        self.k, _ = centers.shape
 
     def get_centers(self):
         return self.centers
@@ -43,39 +54,45 @@ class kMeans:
     def get_labels(self):
         return self.labels
 
-    #the main function to group all samples into k sets.
-    #there are 2 iteration steps.
+
     def discretize(self):
+        """
+        the main function to group all samples into k sets
+        Parameters
+        ----------
+
+        Returns
+        -------
+        centers : numpy.array with the calculated centers
+        labels : numpy.array with lables of each point
+        """
         clusterChanged = True
         N,D = self.data.shape
         if self.centerIsSet is False:
-             #self.centers = np.asarray([self.data[np.random.randint(0, len(self.data))] for _ in xrange(k)], dtype=np.float32)
 		     self.centers = self.__random_centers()
 
         iter = 0
+        k = self.k
+        data = self.data
 
-        while clusterChanged:
+        while (clusterChanged & (iter < self.maxiter)):
             clusterChanged = False
-
+            iter = iter+1
             for i in xrange(N):
-                dmin = float("inf")
-                minIdx = -1
                 #the 1st step of iteration,calculate the distance between each sample and each center
                 #for each sample,do the iteration until find the minimal distance to all k centers
                 #label each sample using the index of the center which is nearest to it.
-                for j in range(self.k):
-                    dist = np.linalg.norm(self.centers[j, :]-self.data[i, :])
-                    if dist < dmin:
-                        dmin = dist
-                        minIdx = j
+
+                minIdx = distance.cdist(self.centers, [data[i, :]]).argmin()
 
                 if self.labels[i, 0] != minIdx:
                     if self.centerIsSet is False:
                         clusterChanged = True
                     self.labels[i, 0] = minIdx
+
             #the 2nd step of iteration,calculate the mean value of each labelled group
             #as new center.
             if self.centerIsSet is False:
-                for i in range(self.k):
-                    pointsInCluster = self.data[np.nonzero(self.labels[:, 0] == i)[0]]
+                for i in range(k):
+                    pointsInCluster = data[np.nonzero(self.labels[:, 0] == i)[0]]
                     self.centers[i, :] = np.mean(pointsInCluster, axis=0)
